16ea450d0c16d12cb375e64e0b953c4d1cc08e0f
GaltieriALMA <alexander.info.gutierrez@gmail.com>
2026-09-23 13:49:45 -0300

    Add Vosk Android dependency to build.gradle.kts

--- alma_files/ALMA_MVP/android_app/app/build.gradle.kts
+++ alma_files/ALMA_MVP/android_app/app/build.gradle.kts
@@ -33,4 +33,5 @@ android {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+implementation("com.alphacephei:vosk-android:0.3.75")
 }
